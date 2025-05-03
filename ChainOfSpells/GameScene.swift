dfbaf35e54e77a9d09e408af696a7f18c4d7a424
amelia citra <ameliacitradewi@gmail.com>
2025-05-03 17:27:18 +0700

    - tap can only be one card at a time - functional attack and discard buttons are already on the card - calculation of remaining cards in the deck card - numbers on cards with array 1-10 in shuffle - card elements with enum

---
+++ ChainOfSpells/GameScene.swift
@@ -0,0 +1,174 @@
+//
+//  GameScene.swift
+//  ChainOfSpells
+//
+//  Created by Amelia Citra on 03/05/25.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+	enum Element: String, CaseIterable {
+		case fire, water, earth, air
+	}
+
+	struct CardData {
+		let element: Element
+		let power: Int
+	}
+
+	var selectedElement: Element = .fire
+	var deck: [CardData] = []
+	var handCards: [SKSpriteNode] = []
+	var playedCount = 0 {
+		didSet {
+			let remaining = max(0, 10 - playedCount)
+			cardCountLabel.text = "\(remaining)/100"
+		}
+	}
+
+	let cardCountLabel = SKLabelNode(fontNamed: "Avenir")
+	var selectedCard: SKSpriteNode?
+	var attackButton: SKLabelNode!
+	var discardButton: SKLabelNode!
+	var enemyHP = 100 {
+		didSet {
+			hpLabel.text = "\(enemyHP)/100"
+			hpBar.xScale = CGFloat(enemyHP) / 100.0
+		}
+	}
+	let hpLabel = SKLabelNode(fontNamed: "Avenir")
+	let hpBar = SKSpriteNode(color: .green, size: CGSize(width: 200, height: 10))
+	let hpBarBackground = SKSpriteNode(color: .red, size: CGSize(width: 200, height: 10))
+
+	override func didMove(to view: SKView) {
+		backgroundColor = .black
+		setupUI()
+		generateDeck(for: selectedElement)
+		drawInitialCards()
+	}
+
+	func setupUI() {
+		// HP Bar
+		hpBar.anchorPoint = CGPoint(x: 0.0, y: 0.5)
+		hpBar.position = CGPoint(x: size.width/2 - 100, y: size.height - 50)
+		hpBarBackground.position = hpBar.position
+		addChild(hpBarBackground)
+		addChild(hpBar)
+
+		hpLabel.fontSize = 20
+		hpLabel.position = CGPoint(x: size.width / 2, y: size.height - 70)
+		hpLabel.text = "\(enemyHP)/100"
+		addChild(hpLabel)
+
+		// Card Count Label
+		cardCountLabel.fontSize = 18
+		cardCountLabel.position = CGPoint(x: size.width - 60, y: 50)
+		cardCountLabel.text = "10/10"
+		addChild(cardCountLabel)
+
+		// Action Buttons
+		attackButton = SKLabelNode(text: "Attack")
+		attackButton.fontName = "Avenir"
+		attackButton.fontSize = 24
+		attackButton.position = CGPoint(x: size.width / 2 - 60, y: 180)
+		attackButton.name = "attackButton"
+		attackButton.isHidden = true
+		addChild(attackButton)
+
+		discardButton = SKLabelNode(text: "Discard")
+		discardButton.fontName = "Avenir"
+		discardButton.fontSize = 24
+		discardButton.position = CGPoint(x: size.width / 2 + 60, y: 180)
+		discardButton.name = "discardButton"
+		discardButton.isHidden = true
+		addChild(discardButton)
+	}
+
+	func generateDeck(for element: Element) {
+		let uniquePowers = Array(1...10).shuffled()
+		for power in uniquePowers {
+			deck.append(CardData(element: element, power: power))
+		}
+	}
+
+	func drawInitialCards() {
+		for i in 0..<5 {
+			drawCard(at: i)
+		}
+	}
+
+	func drawCard(at index: Int) {
+		guard playedCount < 10, !deck.isEmpty else { return }
+		let card = deck.removeFirst()
+		playedCount += 1
+		let cardNode = SKSpriteNode(texture: SKTexture(imageNamed: "card_\(card.element)")) // show card based on element
+		cardNode.size = CGSize(width: 80, height: 120) // Tambahkan ini
+		cardNode.name = "card"
+		cardNode.position = CGPoint(x: CGFloat(150 + (index * 130)), y: 100)
+		cardNode.userData = ["power": card.power, "originalY": 100.0]
+
+		let label = SKLabelNode(text: "\(card.power)")
+		label.fontSize = 16
+		label.fontName = "Avenir"
+		label.position = CGPoint(x: -40, y: 40)
+		label.zPosition = 2
+		cardNode.addChild(label)
+
+		handCards.insert(cardNode, at: index)
+		addChild(cardNode)
+	}
+
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first else { return }
+		let location = touch.location(in: self)
+		let nodes = nodes(at: location)
+
+		if let node = nodes.first(where: { $0.name == "card" }) as? SKSpriteNode {
+			if selectedCard != node {
+				// Turunkan kartu sebelumnya jika ada
+				if let previous = selectedCard {
+					previous.run(SKAction.moveTo(y: 100, duration: 0.1))
+				}
+				// Angkat kartu yang baru dipilih
+				selectedCard = node
+				node.run(SKAction.moveTo(y: 130, duration: 0.1))
+				attackButton.isHidden = false
+				discardButton.isHidden = false
+			}
+		} else if nodes.contains(attackButton), let card = selectedCard {
+			performAttack(with: card)
+		} else if nodes.contains(discardButton), let card = selectedCard {
+			discard(card: card)
+		}
+	}
+
+	func performAttack(with card: SKSpriteNode) {
+		guard let power = card.userData?["power"] as? Int else { return }
+		card.run(SKAction.move(to: CGPoint(x: size.width / 2, y: size.height / 2), duration: 0.3)) {
+			card.removeFromParent()
+		}
+		enemyHP = max(0, enemyHP - power)
+		if let index = handCards.firstIndex(of: card) {
+			handCards.remove(at: index)
+		}
+		cleanupAfterAction()
+	}
+
+	func discard(card: SKSpriteNode) {
+		card.removeFromParent()
+		if let index = handCards.firstIndex(of: card) {
+			handCards.remove(at: index)
+			if playedCount < 10, !deck.isEmpty {
+				drawCard(at: index)
+			}
+		}
+		cleanupAfterAction()
+	}
+
+	func cleanupAfterAction() {
+		selectedCard = nil
+		attackButton.isHidden = true
+		discardButton.isHidden = true
+	}
+}
---
+++ ChainOfSpells/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewController.swift
+//  ChainOfSpells
+//
+//  Created by Amelia Citra on 03/05/25.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Pastikan view bisa dikonversi ke SKView
+		if let view = self.view as? SKView {
+			// Buat scene menggunakan ukuran view
+			let scene = GameScene(size: view.bounds.size)
+			scene.scaleMode = .aspectFill
+			
+			// Tampilkan scene
+			view.presentScene(scene)
+			
+			view.ignoresSiblingOrder = true
+			view.showsFPS = true
+			view.showsNodeCount = true
+		}
+	}
+
+	override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+		if UIDevice.current.userInterfaceIdiom == .phone {
+			return .allButUpsideDown
+		} else {
+			return .all
+		}
+	}
+
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
+}
