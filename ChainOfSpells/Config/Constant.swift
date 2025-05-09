d5a6715e2441d2e831d58899cc1c8a451d57c7d9
Mushafa Fadzan Andira <mushafaandira21@gmail.com>
2025-05-09 08:30:01 +0700

    feat : balancing

--- ChainOfSpells/Config/Constant.swift
+++ ChainOfSpells/Config/Constant.swift
@@ -59,23 +59,23 @@ var stages : [StageModel] =  [
 /// Enemy
 var enemy1 = EnemyModel( name: "enemy_1", folderName: "enemy_skeleton", idleAnimations:  ["skeleton_1","skeleton_2","skeleton_3","skeleton_4"], hp: 10, initialChainEffect: ChainEffectModel(type: .damageReduction, remainingTurn: 1, level: .enemy), loseImage: "skeleton_1")
 
-var enemy2 = EnemyModel( name: "enemy_2", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 20, initialChainEffect: ChainEffectModel(type: .burn, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
+var enemy2 = EnemyModel( name: "enemy_2", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 30, initialChainEffect: ChainEffectModel(type: .burn, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
 
-var enemy3 = EnemyModel( name: "enemy_3", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 30, initialChainEffect: ChainEffectModel(type: .critical, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
+var enemy3 = EnemyModel( name: "enemy_3", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 50, initialChainEffect: ChainEffectModel(type: .critical, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
 
-var enemy4 = EnemyModel( name: "enemy_4", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 40, initialChainEffect: ChainEffectModel(type: .damageReduction, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
+var enemy4 = EnemyModel( name: "enemy_4", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 70, initialChainEffect: ChainEffectModel(type: .damageReduction, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
 
-var enemy5 = EnemyModel( name: "enemy_5", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 50, initialChainEffect: ChainEffectModel(type: .explosion, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
+var enemy5 = EnemyModel( name: "enemy_5", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 90, initialChainEffect: ChainEffectModel(type: .explosion, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
 
-var enemy6 = EnemyModel( name: "enemy_6", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 70, initialChainEffect: ChainEffectModel(type: .mist, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
+var enemy6 = EnemyModel( name: "enemy_6", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 120, initialChainEffect: ChainEffectModel(type: .mist, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
 
-var enemy7 = EnemyModel( name: "enemy_7", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 100, initialChainEffect: ChainEffectModel(type: .regeneration, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
+var enemy7 = EnemyModel( name: "enemy_7", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 150, initialChainEffect: ChainEffectModel(type: .regeneration, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
 
-var enemy8 = EnemyModel( name: "enemy_8", folderName: "BossIdle",idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 150, initialChainEffect: ChainEffectModel(type: .burn, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
+var enemy8 = EnemyModel( name: "enemy_8", folderName: "BossIdle",idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 200, initialChainEffect: ChainEffectModel(type: .burn, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
 
-var enemy9 = EnemyModel( name: "enemy_9", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 200, initialChainEffect: ChainEffectModel(type: .explosion, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
+var enemy9 = EnemyModel( name: "enemy_9", folderName: "BossIdle", idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 250, initialChainEffect: ChainEffectModel(type: .explosion, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
 
-var enemy10 = EnemyModel( name: "enemy_10", folderName: "BossIdle",idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 300, initialChainEffect: ChainEffectModel(type: .mist, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
+var enemy10 = EnemyModel( name: "enemy_10", folderName: "BossIdle",idleAnimations:  ["boss1","boss2","boss3","boss4","boss5","boss6","boss7"], hp: 400, initialChainEffect: ChainEffectModel(type: .mist, remainingTurn: 1, level: .enemy), loseImage: "boss-lose")
 
 
 
